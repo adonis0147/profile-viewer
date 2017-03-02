6df74e90eccfeeb08ada072bc2c09b639ce674c7
Adonis Ling <adonis0147@gmail.com>
2017-03-02 23:14:59 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let win
+
+function createWindow() {
+	win = new BrowserWindow({width: 800, height: 600})
+	win.loadURL(path.join('file://', __dirname, 'index.html'))
+}
+
+app.on('ready', createWindow)
+
